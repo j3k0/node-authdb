19387c26b872fbedd85a389d468f7bf47046247f
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-01-28 15:19:17 +0200

    expire tokens

--- authdb.js
+++ authdb.js
@@ -45,6 +45,7 @@ Client.prototype.getAccount = function(token, cb) {
 // cb(err, reply) will be called with result.
 Client.prototype.addAccount = function(token, account, cb) {
     this.redisClient.set(token, JSON.stringify(account), cb);
+    this.redisClient.expire(token, 3600 * 24 * 30); // token will be valid for 30 days
 };
 
 // Module object
